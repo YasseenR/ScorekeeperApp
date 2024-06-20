ad69ef2c8abcc3ef81202ce879ebcd9e87af87ea
YasseenR <yasseenr@gmail.com>
2024-06-20 15:56:32 -0400

    Initial Commit

---
+++ ScoreKeeperApp/ScoreKeeperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperAppApp.swift
+//  ScoreKeeperApp
+//
+//  Created by Yasseen Rouni on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
