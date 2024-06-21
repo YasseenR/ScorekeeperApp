665fa469136ea7d0bb4a44c24a614af290ca2e66
YasseenR <yasseenr@gmail.com>
2024-06-21 17:23:11 -0400

    Created a new scorekeeping app that keeps track of score between two teams through user input. Basic functionality has been developed. Two score buttons for home and away and a reset button to reset scores.

--- ScoreKeeperApp/ContentView.swift
+++ ScoreKeeperApp/ContentView.swift
@@ -8,14 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var homeScore = 0
+    @State private var awayScore = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color("Mikasa")
+                .ignoresSafeArea()
+            VStack (spacing: 30) {
+                Spacer()
+                Text("Score")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                HStack (spacing: 0) {
+                    ZStack {
+                        Color.blue
+                        VStack {
+                            Text("Home")
+                            Spacer()
+                            Button {
+                                homeScoreAdd()
+                            } label : {
+                                VStack {
+                                    Text("\(homeScore)")
+                                    
+                                }
+                                .frame(maxWidth: 180)
+                                    .padding(.vertical, 60)
+                                .background(.regularMaterial)
+                                .clipShape(.rect(cornerRadius: 20))
+                                .font(.system(size: 64))
+                                .buttonStyle(PlainButtonStyle())
+                            }
+                            
+                            Spacer()
+                            
+                        }
+                    }
+                    .foregroundStyle(.black)
+                    ZStack {
+                        Color.red
+                        VStack {
+                            Text("Away")
+                            Spacer()
+                            Button {
+                                awayScoreAdd()
+                            } label : {
+                                VStack {
+                                    Text("\(awayScore)")
+                                    
+                                }
+                                .frame(maxWidth: 180)
+                                    .padding(.vertical, 60)
+                                .background(.regularMaterial)
+                                .clipShape(.rect(cornerRadius: 20))
+                                .font(.system(size: 64))
+                            }
+                            Spacer()
+                        }
+                        
+                    }
+                    .foregroundStyle(.black)
+                }
+                ZStack {
+                    Color("Mikasa")
+                    VStack {
+                        Button {
+                            scoreReset()
+                        } label : {
+                            ZStack {
+                                Color(.white)
+                                VStack {
+                                    Text("Reset")
+                                    
+                                }
+                            }
+                            .frame(maxWidth: 180, maxHeight: 90)
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)                        }
+                        
+                    }
+                    
+                }
+                .foregroundStyle(.green)
+            }
         }
-        .padding()
+    }
+    func homeScoreAdd() {
+        
+        homeScore += 1
+        
+    }
+    
+    func awayScoreAdd() {
+        
+        awayScore += 1
+    }
+    
+    func scoreReset() {
+        awayScore = 0
+        homeScore = 0
     }
 }
 
