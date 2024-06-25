56417f995b607ba3347b386585173a3cedf1f18c
YasseenR <yasseenrouni@gmail.com>
2024-06-25 13:25:53 -0400

    Test

--- ScoreKeeperApp/ContentView.swift
+++ ScoreKeeperApp/ContentView.swift
@@ -73,7 +73,7 @@ struct ContentView: View {
                                 }, buttonText: "-")
                                 
                                 MiscButton(buttonAction: {// action here
-                                }, buttonText: "0")
+                                }, buttonText: "1")
                                 
                             }
                             
