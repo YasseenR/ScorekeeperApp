ae91a9ebda45196858cc47073863e3b58f51c00f
YasseenR <yasseenrouni@gmail.com>
2024-11-25 16:43:07 -0500

    Improving layout of buttons.

--- ScoreKeeperApp/ContentView.swift
+++ ScoreKeeperApp/ContentView.swift
@@ -85,177 +85,8 @@ struct ContentView: View {
     
     var body: some View {
         LandscapeView()
-        /*Group {
-            if isLandscape {
-                LandscapeView()
-            } else {
-                PortraitView()
-            }
-        }
-        .onAppear {
-            isLandscape = UIDevice.current.orientation.isLandscape
-        }
-        .detectOrientationChange()
-        .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-            isLandscape = UIDevice.current.orientation.isLandscape
-        }*/
     }
 }
-
-/*
-struct PortraitView: View {
-    @State private var homeScore = 0
-    @State private var awayScore = 0
-    
-    
-    var body: some View {
-        ZStack {
-            Color(.white)
-                .ignoresSafeArea()
-            VStack (spacing: 30) {
-                Spacer()
-                Text("Score")
-                    .font(.largeTitle.weight(.bold))
-                    .foregroundStyle(.black)
-                HStack (spacing: 0) {
-                    ZStack {
-                        Color(.white)
-                        VStack {
-                            Spacer()
-                            Text("Home")
-                                .foregroundStyle(.black)
-                                .font(.system(size: 24))
-                            Spacer()
-                            Button {
-                                homeScoreAdd()
-                            } label : {
-                                VStack {
-                                    Text("\(homeScore)")
-                                    
-                                }
-                                .frame(maxWidth: 180)
-                                .padding(.vertical, 120)
-                                .background(.blue)
-                                .clipShape(.rect(cornerRadius: 20))
-                                .font(.system(size: 96))
-                                .buttonStyle(PlainButtonStyle())
-                                
-                            }
-                            HStack (spacing: 75){
-                                
-                                MiscButton(buttonAction: {
-                                    homeScoreSubtract()
-                                }, buttonText: "-")
-                                
-                                MiscButton(buttonAction: {// action here
-                                }, buttonText: "1")
-                                
-                            }
-                            
-                            Spacer()
-                            Spacer()
-                            
-                        }
-                    }
-                    .foregroundStyle(.black)
-                    ZStack {
-                        Color(.white)
-                        VStack {
-                            Spacer()
-                            Text("Away")
-                                .foregroundStyle(.black)
-                                .font(.system(size: 24))
-                            Spacer()
-                            Button {
-                                awayScoreAdd()
-                            } label : {
-                                VStack {
-                                    Text("\(awayScore)")
-                                    
-                                }
-                                .frame(maxWidth: 180)
-                                    .padding(.vertical, 120)
-                                .background(.red)
-                                .clipShape(.rect(cornerRadius: 20))
-                                .font(.system(size: 96))
-                            }
-                            HStack (spacing: 75){
-                                
-                                
-                                MiscButton(buttonAction: {// action here
-                                }, buttonText: "0")
-                                
-                                MiscButton(buttonAction: {
-                                    awayScoreSubtract()
-                                }, buttonText: "-")
-                                
-                                
-                                
-                            }
-                            Spacer()
-                            Spacer()
-                        }
-                        
-                        
-                    }
-                    .foregroundStyle(.black)
-                }
-                ZStack {
-                    Color(.white)
-                    VStack {
-                        Button {
-                            scoreReset()
-                        } label : {
-                            ZStack {
-                                Color("Mikasa")
-                                VStack {
-                                    Text("Reset All")
-                                    
-                                }
-                                .foregroundColor(.white)
-                            }
-                            .frame(maxWidth: 180, maxHeight: 90)
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)                        }
-                        
-                    }
-                    
-                }
-                .foregroundStyle(.green)
-            }
-        }
-    }
-    func homeScoreAdd() {
-        
-        homeScore += 1
-        
-    }
-    
-    func homeScoreSubtract() {
-        
-        if (homeScore > 0) {
-            homeScore -= 1
-        }
-
-    }
-    
-    func awayScoreAdd() {
-        
-        awayScore += 1
-    }
-    
-    func awayScoreSubtract() {
-        if (awayScore > 0) {
-            awayScore -= 1
-        }
-    }
-    
-    func scoreReset() {
-        awayScore = 0
-        homeScore = 0
-    }
-}
-*/
 
 struct ColorOptions {
     let options: [(key: String, value: Image)] = [
@@ -392,139 +223,143 @@ struct LandscapeView: View {
                 .ignoresSafeArea()
             VStack {
                 ZStack {
-                    HStack {
-                        Button {
-                            self.showingSettings.toggle()
-                        } label: {
-                            Image(systemName: "gearshape.fill")
-                        }
-                        .font(.system(size: 30))
-                        .sheet(isPresented: $showingSettings) {
-                            SettingsView(currentHomeColor: $homeColor, currentAwayColor: $awayColor, showingSettings: $showingSettings, homeTeamName: $homeTeamName, awayTeamName: $awayTeamName)
-                        }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Spacer()
-                        Text(homeTeamName)
-                        Spacer()
-                        Spacer()
-                        Text(awayTeamName)
-                        Spacer()
-                    }
-                }
-                HStack{
-                    Button {
-                        homeScoreAdd()
-                    } label : {
-                        VStack {
-                            Text("\(homeScore)")
-                                .font(Font.custom("Exo 2", size: 96))
-                        }
-                        .frame(maxWidth: 360, maxHeight: 50)
-                        .padding(.vertical, 100)
-                        .background(Color(homeColor))
-                        .clipShape(.rect(cornerRadius: 20))
-                        .font(.system(size: 96))
-                        .buttonStyle(PlainButtonStyle())
-                        .foregroundColor(.black)
-                    }
-                    
-                    Button {
-                        awayScoreAdd()
-                    } label : {
-                        VStack {
-                            Text("\(awayScore)")
-                                .font(Font.custom("Exo 2", size: 96))
-                            
-                        }
-                        .frame(maxWidth: 360, maxHeight:50)
-                        .padding(.vertical, 100)
-                        .background(Color(awayColor))
-                        .clipShape(.rect(cornerRadius: 20))
-                        .font(.system(size: 96))
-                        .buttonStyle(PlainButtonStyle())
-                        .foregroundColor(.black)
-                    }
-                }
-                HStack {
-                    HStack {
-                        MiscButton(buttonAction: {
-                            homeScoreSubtract()
-                        }, buttonText: "-")
-                        Button {
-                            homeScore = 0
-                        } label: {
-                            Image(systemName: "arrow.uturn.backward.circle.fill")
-                        }
-                        .foregroundStyle(Color("Mikasa"))
-                        .font(.system(size: 30))
-                    }
-                    
-                    Spacer()
                     VStack {
-                        Picker("Set Score", selection: $setScoreHome) {
-                            ForEach(0..<5, id:\.self) { score in
-                                Text("\(score)")
+                        ZStack {
+                            HStack {
+                                Button {
+                                    self.showingSettings.toggle()
+                                } label: {
+                                    Image(systemName: "gearshape.fill")
+                                }
+                                .font(.system(size: 30))
+                                .sheet(isPresented: $showingSettings) {
+                                    SettingsView(currentHomeColor: $homeColor, currentAwayColor: $awayColor, showingSettings: $showingSettings, homeTeamName: $homeTeamName, awayTeamName: $awayTeamName)
+                                }
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                Spacer()
+                                Text(homeTeamName)
+                                Spacer()
+                                Spacer()
+                                Text(awayTeamName)
+                                Spacer()
                             }
                         }
-                            .labelsHidden()
-                            .frame(minWidth: 50, maxHeight: 25)
-                            .padding(.vertical, 5)
-                            .background(Color("Mikasa"))
-                            .foregroundColor(.white)
-                            .clipShape(.rect(cornerRadius:20))
-                            .foregroundStyle(.white)
-                            .accentColor(.white)
+                        
+                        HStack{
+                            Button {
+                                homeScoreAdd()
+                            } label : {
+                                VStack {
+                                    Text("\(homeScore)")
+                                        .font(Font.custom("Exo 2", size: 96))
+                                }
+                                .frame(maxWidth: 360, maxHeight: 50)
+                                .padding(.vertical, 100)
+                                .background(Color(homeColor))
+                                .clipShape(.rect(cornerRadius: 20))
+                                .font(.system(size: 96))
+                                .buttonStyle(PlainButtonStyle())
+                                .foregroundColor(.black)
+                            }
+                            
+                            Button {
+                                awayScoreAdd()
+                            } label : {
+                                VStack {
+                                    Text("\(awayScore)")
+                                        .font(Font.custom("Exo 2", size: 96))
+                                    
+                                }
+                                .frame(maxWidth: 360, maxHeight:50)
+                                .padding(.vertical, 100)
+                                .background(Color(awayColor))
+                                .clipShape(.rect(cornerRadius: 20))
+                                .font(.system(size: 96))
+                                .buttonStyle(PlainButtonStyle())
+                                .foregroundColor(.black)
+                            }
                         }
-                    Button {
-                        scoreReset()
-                    } label : {
-                        ZStack {
-                            Color("Mikasa")
+                        HStack {
+                            HStack {
+                                MiscButton(buttonAction: {
+                                    homeScoreSubtract()
+                                }, buttonText: "-")
+                                Button {
+                                    homeScore = 0
+                                } label: {
+                                    Image(systemName: "arrow.uturn.backward.circle.fill")
+                                }
+                                .foregroundStyle(Color("Mikasa"))
+                                .font(.system(size: 30))
+                            }
+                            
+                            Spacer()
                             VStack {
-                                Text("Reset All")
+                                Picker("Set Score", selection: $setScoreHome) {
+                                    ForEach(0..<5, id:\.self) { score in
+                                        Text("\(score)")
+                                    }
+                                }
+                                .labelsHidden()
+                                .frame(minWidth: 50, maxHeight: 25)
+                                .padding(.vertical, 5)
+                                .background(Color("Mikasa"))
+                                .foregroundColor(.white)
+                                .clipShape(.rect(cornerRadius:20))
+                                .foregroundStyle(.white)
+                                .accentColor(.white)
+                            }
+                            Button {
+                                scoreReset()
+                            } label : {
+                                ZStack {
+                                    Color("Mikasa")
+                                    VStack {
+                                        Text("Reset All")
+                                        
+                                    }
+                                    .foregroundColor(.white)
+                                }
+                                .frame(maxWidth: 90, maxHeight: 45)
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                            }
+                            VStack {
+                                Picker("Set Score", selection: $setScoreAway) {
+                                    ForEach(0..<5, id:\.self) { score in
+                                        Text("\(score)")
+                                    }
+                                }
+                                .frame(minWidth: 50, maxHeight: 25)
+                                .padding(.vertical, 5)
+                                .background(Color("Mikasa"))
+                                .foregroundColor(.white)
+                                .clipShape(.rect(cornerRadius:20))
+                                .foregroundStyle(.white)
+                                .accentColor(.white)
+                            }
+                            Spacer()
+                            
+                            HStack {
+                                
+                                Button {
+                                    awayScore = 0
+                                } label: {
+                                    Image(systemName: "arrow.uturn.backward.circle.fill")
+                                }
+                                .foregroundStyle(Color("Mikasa"))
+                                .font(.system(size: 30))
+                                MiscButton(buttonAction: {
+                                    awayScoreSubtract()
+                                }, buttonText: "-")
                                 
                             }
-                            .foregroundColor(.white)
                         }
-                        .frame(maxWidth: 90, maxHeight: 45)
-                        .clipShape(.capsule)
-                        .shadow(radius: 5)
-                    }
-                    VStack {
-                        Picker("Set Score", selection: $setScoreAway) {
-                            ForEach(0..<5, id:\.self) { score in
-                                Text("\(score)")
-                            }
-                        }
-                        .frame(minWidth: 50, maxHeight: 25)
-                            .padding(.vertical, 5)
-                            .background(Color("Mikasa"))
-                            .foregroundColor(.white)
-                            .clipShape(.rect(cornerRadius:20))
-                            .foregroundStyle(.white)
-                            .accentColor(.white)
-                        }
-                    Spacer()
-                    
-                    HStack {
-                        
-                        Button {
-                            awayScore = 0
-                        } label: {
-                            Image(systemName: "arrow.uturn.backward.circle.fill")
-                        }
-                        .foregroundStyle(Color("Mikasa"))
-                        .font(.system(size: 30))
-                        MiscButton(buttonAction: {
-                            awayScoreSubtract()
-                        }, buttonText: "-")
-                        
                     }
                 }
-                
             }
         }
         .foregroundStyle(.black)
